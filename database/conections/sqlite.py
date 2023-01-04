2990166fc2f113ac54f85e8ea4d5d769cdc3333f
Wesley Steve <wesley.silva23@hotmail.com>
2023-01-04 05:21:47 -0300

    feat: sqlite, add function connect_db_sqlite

--- database/conections/sqlite.py
+++ database/conections/sqlite.py
@@ -15,3 +15,21 @@ def create_db_sqlite(path, nome_banco):
     con = sqlalchemy.create_engine(str_conexao)
 
     return con
+  
+  
+def connect_db_sqlite(db_path):
+    """Function to connect with local sqlite
+
+    Returns:
+        con: returns a connection sqlite database
+    """
+
+    # define connection string with sqlite database
+    str_conexao = "sqlite:///{db_path}"
+
+    str_conexao = "sqlite:///{path}".format(path=os.path.join(db_path))
+
+    # create connection to sqlite database
+    con = sqlalchemy.create_engine(str_conexao)
+
+    return con
