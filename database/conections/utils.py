fcf54f67587424aa2b6279e80e76b941c28daec9
Wesley Steve <wesley.silva23@hotmail.com>
2023-01-04 05:10:13 -0300

    feat: utils, add function execute_many_sql

--- database/conections/utils.py
+++ database/conections/utils.py
@@ -12,3 +12,17 @@ def import_query(path, **kwargs):
     with open(path, "r", **kwargs) as file_query:
         query = file_query.read()
     return query
+
+
+
+def execute_many_sql(sql, con):
+    """Function that receives a sql connection and verbose (optional) and executes
+
+    Args:
+        sql (command): parameter that receive the query  
+        con (parameter): conection string as database sqlite
+    """
+
+    for i in sql.split(";")[:-1]:
+        con.execute(i)
+        
